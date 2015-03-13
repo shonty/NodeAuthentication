0d05870bccbf580a6f4ce13415de63b63df5836b
Shane McHugh <shane.mchugh@wonga.com>
2015-03-13 12:30:43 +0000

    adding shell of authentication app. with home page

---
+++ src/app/routes.js
@@ -0,0 +1,40 @@
+module.exports = function(app, passport) {
+
+    app.get('/', function(req, res) {
+        res.render('index.ejs'); // load the index.ejs file
+    });
+
+    app.get('/login', function(req, res) {
+        res.render('login.ejs', { message: req.flash('loginMessage') });
+    });
+
+    // process the login form
+    // app.post('/login', do all our passport stuff here);
+
+    app.get('/signup', function(req, res) {
+        res.render('signup.ejs', { message: req.flash('signupMessage') });
+    });
+
+    // process the signup form
+    // app.post('/signup', do all our passport stuff here);
+
+    app.get('/profile', isLoggedIn, function(req, res) {
+        res.render('profile.ejs', {
+            user : req.user
+        });
+    });
+
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+};
+
+function isLoggedIn(req, res, next) {
+
+    if (req.isAuthenticated()) {
+        return next();
+      }
+      
+    res.redirect('/');
+}
